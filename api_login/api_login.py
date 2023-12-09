32bad3a76993501a83d4d32bbe9ae0b3df0e4e98
Ekaterina Zvereva <evzvereva1@gmail.com>
2023-12-10 00:46:06 +0500

    Correction code login api in api_login/api_login.py

--- api_login/api_login.py
+++ api_login/api_login.py
@@ -1,14 +1,12 @@
 import streamlit as st
 
-
 def api_login():
     """
     Функция позволяет вызвать функцию upload_login_app_setting() открыть и прочитать файл с расширением .yaml с
     настройками для подключения к API GPT бота
     """
     api_json = {"yandexGPT": {
-        "api_key": ""
-                   "-OwAmM6dZmWMfybxKPTni!hcY85c9CYmt1",
+        "api_key": "",
         "prompt": "",
         "history": []}}
 
