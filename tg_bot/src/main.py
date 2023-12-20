eebd08eed4674dcb19748964cd28b9555e4231fb
Aleksandr Mulyavin <147722577+aleksandr-mulyavin@users.noreply.github.com>
2023-12-20 16:12:42 +0500

    Update main.py Добавление комментариев

--- tg_bot/src/main.py
+++ tg_bot/src/main.py
@@ -13,16 +13,25 @@ logger = logging.getLogger(__name__)
 
 
 async def main() -> None:
+    """
+    Настройка и запуск ТГ бота
+    """
+
+    # Чтение конфигурации
     config: Config = load_config()
 
+    # Создание и настройка бота
     bot = Bot(token=config.tg_bot_config.token,
               parse_mode=ParseMode.HTML)
+    # Очистка очереди сообщений (пришедших пока бот был выключен)
     await bot.delete_webhook(drop_pending_updates=True)
 
+    # Запуск диспетчера в режиме опроса сервера
     dispatcher = Dispatcher(storage=MemoryStorage())
     dispatcher.include_router(bot_router)
     await dispatcher.start_polling(bot)
 
 
+# Обработка запуска программы как PY файла
 if __name__ == '__main__':
     asyncio.run(main())
